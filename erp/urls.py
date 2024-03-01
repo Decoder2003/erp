248bbb3cb234029ae2f08002c9837fb1fa0f8a9a
Dhruv Kalra <kalradhruv2003@gmail.com>
2024-03-01 09:36:20 +0530

    deployment bug : 404

--- erp/urls.py
+++ erp/urls.py
@@ -22,7 +22,7 @@ from .router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls)),
+    path('/',include(router.urls)),
 ]
 
 if settings.DEBUG:  
