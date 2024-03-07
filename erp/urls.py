079d44e69d6323de1dd98ff20be9e2812b4d8049
Dhruv Kalra <kalradhruv2003@gmail.com>
2024-03-07 18:33:08 +0530

    route bug fixed

--- erp/urls.py
+++ erp/urls.py
@@ -22,7 +22,7 @@ from .router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include(router.urls)),
+    path('api/',include(router.urls)),
 ]
 
 if settings.DEBUG:  
